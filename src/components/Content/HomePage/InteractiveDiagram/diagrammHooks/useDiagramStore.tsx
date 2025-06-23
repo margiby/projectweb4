6dc84357b3a76103bdc6fa5c8a8ecab1439c052f
Margarita <131612561+margiby@users.noreply.github.com>
2025-06-23 08:22:13 +0200

    Add files via upload

---
+++ src/components/Content/HomePage/InteractiveDiagram/diagrammHooks/useDiagramStore.tsx
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+type Store = {
+  diagramId: string;
+  setDiagramId: (id: string) => void;
+};
+
+/**
+ * Zustand Store für die Verwaltung des aktuellen Diagramm-IDs.
+ * Dieser Store ermöglicht es, die ID des aktuell angezeigten Diagramms zu setzen und abzurufen.
+ * Er wird in der gesamten Anwendung verwendet, um den Zustand des Diagramms zu verfolgen.
+ */
+export const useDiagramStore = create<Store>((set) => ({
+  diagramId: "root",
+  setDiagramId: (id) => set({ diagramId: id }),
+}));
